3fa2a1153eb27a3b202c3f8a9220158f07dc67d8
Sailesh1993 <karki.saileshnep@gmail.com>
2023-03-27 14:21:52 +0300

    New Changes

---
+++ js/Data.js
@@ -0,0 +1,99 @@
+let shopItemsData = [
+  
+    {
+        id: "asd",
+        name: "Burger",
+        price: 15,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/burger.jpg",
+        cat:"American"
+      },
+      {
+        id: "fgh",
+        name: "Biryani",
+        price: 30,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/biryani.jpg",
+        cat:"Indian"
+      },
+      {
+        id: "jkl",
+        name: "pizza",
+        price: 20,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/pizza.jpg",
+        cat:"Italian"
+      },
+      {
+        id: "qwe",
+        name: "pasta",
+        price: 10,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/pasta.jpg",
+        cat:"Italian"
+      },
+      {
+        id: "xcv",
+        name: "Burger",
+        price: 15,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/burger.jpg",
+        cat:"American"
+      },
+      {
+        id: "wer",
+        name: "Naan",
+        price: 30,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/biryani.jpg",
+        cat:"Indian"
+      },
+      {
+        id: "rtk",
+        name: "Fries",
+        price: 20,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/pizza.jpg",
+        cat:"American"
+      },
+      {
+        id: "vro",
+        name: "Noodles",
+        price: 10,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/pasta.jpg",
+        cat:"Chineese"
+      },
+      {
+        id: "mnb",
+        name: "Shrimps",
+        price: 15,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/burger.jpg",
+        cat:"Chineese"
+      },
+      {
+        id: "hgf",
+        name: "American pizza",
+        price: 30,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/biryani.jpg",
+        cat:"American"
+      },
+      {
+        id: "rint",
+        name: "pizza",
+        price: 20,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/pizza.jpg",
+        cat:"Indian"
+      },
+      {
+        id: "kent",
+        name: "pasta",
+        price: 10,
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        img: "images/pasta.jpg",
+        cat:"Italian"
+      },
+];
